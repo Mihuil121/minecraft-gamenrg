0d1e91376d15195a1a8e77ce5610f3f2d7304d99
Mihuil121 <98514571+Mihuil121@users.noreply.github.com>
2024-12-27 15:47:59 +0300

    Update Videos.tsx

--- src/components/Videos/Videos.tsx
+++ src/components/Videos/Videos.tsx
@@ -21,18 +21,21 @@ const Videos = () => {
                     {video.slice().reverse().map((videos: IVideo, index: number) => (
                         <div className="content-V" key={index} >
                             <div className="contentImg" onMouseEnter={mouseOn} onMouseLeave={mouseF}>
-                                {useFrame ? (
-                                    <iframe src={videos.url}
-                                        frameborder="0"
-                                        allowfullscreen="1"
-                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                                        className="iframe-v"></iframe>
-                                ) : (
-                                    <Image
-                                        src={videos.img}
-                                        alt={`${videos.img}`}
-                                    />
-                                )}
+                               {useFrame ? (
+    <iframe 
+        src={videos.url}
+        frameBorder="0" // Corrected from frameborder to frameBorder
+        allowFullScreen={true} // Corrected from allowfullscreen to allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        className="iframe-v"
+    ></iframe>
+) : (
+    <Image
+        src={videos.img}
+        alt={`${videos.img}`}
+    />
+)}
+
                             </div>
                             <div className="content-text-v">
                                 <h1>
