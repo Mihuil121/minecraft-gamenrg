c67c6bc0dbb6e6880911f57429b8823ad58f6772
Mihuil121 <bezugliymicha@gmail.com>
2024-12-29 18:30:25 +0300

    fixe : Video.tsx

--- src/components/Videos/Videos.tsx
+++ src/components/Videos/Videos.tsx
@@ -5,27 +5,32 @@ import Image from "next/image";
 import './videoStyle.scss'
 
 const Videos = () => {
-    const [useFrame, setFrame] = useState<boolean>(false);
+    const [useFrame, setFrame] = useState<boolean[]>(Array(video.length).fill(false));
 
-    const mouseOn = () => {
-        setFrame(true)
+    const mouseOn = (index: number) => {
+        const newFrame = [...useFrame];
+        newFrame[index] = true;
+        setFrame(newFrame);
     }
 
-    const mouseF = () => {
-        setFrame(false)
+    const mouseF = (index: number) => {
+        const newFrame = [...useFrame];
+        newFrame[index] = false;
+        setFrame(newFrame);
     }
+
     return (
         <div>
             <div className="Video">
                 <div className="video-content">
                     {video.slice().reverse().map((videos: IVideo, index: number) => (
                         <div className="content-V" key={index} >
-                            <div className="contentImg" onMouseEnter={mouseOn} onMouseLeave={mouseF}>
-                                {useFrame ? (
+                            <div className="contentImg" onMouseEnter={() => mouseOn(index)} onMouseLeave={() => mouseF(index)}>
+                                {useFrame[index] ? (
                                     <iframe
                                         src={videos.url}
-                                        frameBorder="0" // Corrected from frameborder to frameBorder
-                                        allowFullScreen={true} // Corrected from allowfullscreen to allowFullScreen
+                                        frameBorder="0" 
+                                        allowFullScreen={true} 
                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                                         className="iframe-v"
                                     ></iframe>
@@ -35,7 +40,6 @@ const Videos = () => {
                                         alt={`${videos.img}`}
                                     />
                                 )}
-
                             </div>
                             <div className="content-text-v">
                                 <h1>
